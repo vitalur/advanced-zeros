48fa705cc73bf3ae71202100b42a0630a8c68390
vitalur <39513998+vitalur@users.noreply.github.com>
2018-10-04 16:07:23 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,59 +1,37 @@
 module.exports = function getZerosCount(number, base) {
-	
-	let num, count, n;
-    let array =[], counter = [], count_array = [], results = [];
-
+    
+    let power, result, coun = [], array = [], results = [];
+    
     for (let i = 2; i < base; i++) {
         while(base % i === 0) {
             base /= i;
-            counter.push(i);
-            array.push(i);
+            coun.push(i);
         }
     }
 
-    if(base > 1 && base != counter[counter.length - 1]) {
-        counter.push(base);
-        array.push(base);
-    }
-
-    for (let i = 0; i < counter.length - 1; i++) {
-        if (counter[i] == counter[i + 1]) {
-            counter.splice(i, 1);
-            i-=1;
-        }
-    }
-
-    for (let i = 0, len = counter.length; i < len; i++) {
-        let p = pow(array, counter[i]);
-        count_array.push(p);
-    }
-
-	for (let i = 0, len = counter.length; i < len; i++) {
-        n = counter[i];
-        count = 0;
-        num = 1;
-        let qwerty = count_array[i];
-        while (Math.pow(n, num) < number) {
-            count += Math.floor(number / Math.pow(n, num));
-            num++;
-        }
-        count = Math.floor(count / qwerty);
-        results.push(count);
-    }
-
-    let result = results[0];
-    for (let i = 0, len = results.length; i < len; i++) {
-        if(results[i] < result) result = results[i];
-    }
+    if(base != 1) coun.push(base);
     
-    return result; 
-
-	function pow(arr=[], counter)
-    {
-       let pow = 0;
-        for (let i = 0, len = arr.length; i < len; i++) {
-            if(arr[i] === counter) pow++;
+    for (let i = 0, len = coun.length; i < len; i++) {
+        let pow = 0;
+        for (let j = 0, len = coun.length; j < len; j++) {
+            if(coun[j] === coun[i]) pow++;
         }
-        return pow;
+        array.push(pow);
     }
+
+    for (let i = 0, len = coun.length; i < len; i++) {
+        result = 0;
+        power = 1;
+        while (Math.pow(coun[i], power) < number) {
+            result += Math.floor(number / Math.pow(coun[i], power++));  
+        }
+        result = Math.floor(result / array[i]);
+        results.push(result);
+    }
+
+    result = results[0];
+    for (let i = 1, len = results.length; i < len; i++) {
+        results[i] < result ? result = results[i] : result
+    }
+    return result; 
 }
